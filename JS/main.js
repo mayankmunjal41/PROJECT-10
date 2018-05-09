37ccacdb77baa98b35f104021bcf417ba457bd4d
Mayank Munjal <mayankmunjal95@gmail.com>
2018-05-09 15:29:31 +0530

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,64 @@
+
+
+$.ajax({
+  url: 'https://randomuser.me/api/?results=12',
+  dataType: 'json',
+  success: function(data) {
+    function capitalize (string) {
+      // return string.charAt(0).toUpperCase() + string.slice(1);
+      return string.replace(/\b\w/g, function(l){ return l.toUpperCase() })
+    }
+    var randomHtml = '<ul>';
+    $.each(data.results, function (i, photo) {
+
+      randomHtml += '<div class="flex-layout">';
+      randomHtml += '<div class="image">';
+      randomHtml += '<img src ="' + photo.picture.medium + '"class="img">';
+      randomHtml += '</div>';
+      randomHtml += '<div class="li">';
+      randomHtml += '<li>' + capitalize(photo.name.first)+ ' ' + capitalize(photo.name.last) + '</li>';
+      randomHtml += '<li>' + photo.email + '</li>';
+      randomHtml += '<li>' + capitalize(photo.location.state) + '</li>';
+      randomHtml += '</div>';
+      randomHtml += '</div>';
+    });
+    randomHtml += '</ul>';
+    $('.photo').html(randomHtml);
+    var employees = document.querySelectorAll('.flex-layout');
+    for ( var i = 0; i < employees.length; i++) {
+      let obj = data.results[i];
+      employees[i].addEventListener('click', function () {
+        $('.modal').css("display", "block");
+        $('body').css("background-color", "rgba(140, 140, 140, 0.79)");
+        $('.flex-layout').css("background-color", "rgba(193, 191, 191, 0.42)");
+        $('.flex-layout').css("border-color", "grey");
+        let modalContent = `
+          <div class="close">
+          <p>X</p>
+          </div>
+          <div class="modal-content">
+          <img src = ${obj.picture.large} class="image-2">
+          <p class="name">${capitalize(obj.name.first)} ${capitalize(obj.name.last)}</p>
+          <p>${obj.email}</p>
+          <p>${capitalize(obj.location.city)}</p>
+          <hr>
+          <p>${obj.phone}</p>
+          <p>${capitalize(obj.location.street)}, ${ obj.location.postcode}</p>
+          <p>Birthday: ${obj.dob.slice(0,9)}</p>
+          </div>`;
+          $('.modal').html(modalContent);
+          $('.close').click(function() {
+            $('.modal').css("display", "none");
+            $('body').css("background-color", "#9491910a");
+            $('.flex-layout').css("background-color", "white");
+            $('.flex-layout').css("border-color", "lightgrey");
+          })
+
+    })
+
+
+
+  }
+
+  }
+});
